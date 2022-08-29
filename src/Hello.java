b370c533362bbf4eb54d69a0ab610636c727d30a
arian <arian@Envy.unl.edu>
2022-08-29 16:26:09 -0500

    Avery Commit

--- src/Hello.java
+++ src/Hello.java
@@ -10,6 +10,8 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Avery Monson");
 		System.out.println("Computer Engineering");
+		System.out.println("Ariana Shelton");
+		System.out.println("Computer Science");
 	}
 
 }
